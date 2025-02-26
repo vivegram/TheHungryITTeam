62f583dfbb879d397cee1ef084d05620a36d45d5
vivegram <betagalvan@gmail.com>
2025-02-26 15:50:51 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-ReactDOM.render(<LunchPlannerApp />, document.getElementById('root'));
+// index.js
+
+// If you're using modules/imports (with a build system)
+import React from 'react';
+import ReactDOM from 'react-dom';
+import LunchPlannerApp from './app.js';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <LunchPlannerApp />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
